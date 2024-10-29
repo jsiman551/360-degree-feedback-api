cb05f05353d4d5367ad558c7b73ca1de99371c51
Jose Simancas <josesimancas51@gmail.com>
2024-10-29 16:21:58 -0500

    Fix: users registering should be handled only by admins

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,11 +2,17 @@ import express from 'express';
 import { loginUser, registerUser } from '../controllers/authController';
 import validateResource from '../middlewares/validateResource';
 import { createUserSchema, loginUserSchema } from '../schemas/userSchema';
+import { authorizeRoles } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
 // register user route
-router.post('/register', validateResource(createUserSchema), registerUser);
+router.post(
+    '/register',
+    authorizeRoles(['Admin']),
+    validateResource(createUserSchema),
+    registerUser
+);
 
 // login route
 router.post('/login', validateResource(loginUserSchema), loginUser);
